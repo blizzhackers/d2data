be115767be74012de44ae0c0d840858fede5bf30
Nishimura-Katsuo <38635886+Nishimura-Katsuo@users.noreply.github.com>
2022-05-28 13:58:56 -0600

    Visual tweaks for runeword browser.

--- docs/runewords/src/App.vue
+++ docs/runewords/src/App.vue
@@ -146,43 +146,47 @@ const runewords = computed(() => {
 </script>
 
 <template>
-  <div class="row avquest">
+  <div class="row">
     <div class="col-auto">
-      <label class="form-label"><strong>Runes<br>Used</strong></label>
-      <div v-for="rune in Object.keys(data.usedRunes).sort().reverse()" :key="rune" class="my-1">
+      <label class="form-label text-center d-block"><strong>Runes<br>Used</strong></label>
+      <div v-for="rune in Object.keys(data.usedRunes).sort().reverse()" :key="rune" class="my-1 avquest">
         <button :class="{
           'btn': true,
-          'btn-outline-light': data.usedRunes[rune],
+          'btn-outline-warning': data.usedRunes[rune],
           'btn-outline-secondary': !data.usedRunes[rune],
           'fw-bold': true,
         }" style="width:4.25em" @click="() => {
           data.usedRunes[rune] = !data.usedRunes[rune];
-        }">{{ strings[rune + 'L'] }}</button>
+        }" @focus="e => e.target.blur()">{{ strings[rune + 'L'] }}</button>
       </div>
     </div>
     <div class="col">
       <div class="row">
         <div class="col-12 col-lg-6 col-xxl-3 mb-3 mb-xxl-0">
+          <label class="form-label">Runeword</label>
           <input type="search" class="form-control bg-secondary" placeholder="Search a runeword name..." v-model="data.search.name">
         </div>
         <div class="col-12 col-lg-6 col-xxl-3 mb-3 mb-xxl-0">
+          <label class="form-label">Item Type</label>
           <input type="search" class="form-control bg-secondary" placeholder="Search an item type..." v-model="data.search.types">
         </div>
         <div class="col-12 col-lg-6 col-xxl-3 mb-3 mb-lg-0 mb-xxl-0">
+          <label class="form-label">Base Item</label>
           <input type="search" class="form-control bg-secondary" placeholder="Search a base item..." v-model="data.search.items">
         </div>
         <div class="col-12 col-lg-6 col-xxl-3 mb-0 mb-xxl-0">
+          <label class="form-label">Item Modifier</label>
           <input type="search" class="form-control bg-secondary" placeholder="Search an item modifier..." v-model="data.search.mods">
         </div>
       </div>
       <div class="row">
-        <div v-for="runeword in runewords" :key="runeword.Name" class="col-12 col-lg-6 col-xl-4 text-center p-3">
+        <div v-for="runeword in runewords" :key="runeword.Name" class="col-12 col-lg-6 col-xl-4 text-center p-3 runeword-display">
           <div class="unique-title">{{ runeword['*Rune Name'] }}</div>
-          <div class="text-info">
+          <div class="rune-list">{{ runeword.runes.map(rune => strings[rune + 'L']).join(' ') }}</div>
+          <div class="item-type-list my-2">
             <template v-for="(type, index) in runeword.types"><template v-if="index">, </template>{{ itemtypes[type].ItemType }}</template>
           </div>
-          <div class="text-warning">{{ runeword.runes.map(rune => strings[rune + 'L']).join(' ') }}</div>
-          <div class="text-primary">
+          <div class="magic-mod">
             <div v-for="(mod, index) in runeword.mods" :key="index">{{ mod.property['*Tooltip'] }}</div>
           </div>
         </div>
@@ -193,6 +197,11 @@ const runewords = computed(() => {
 
 <style>
   @import "https://blizzhackers.github.io/d2data/css/d2font.css";
+  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+  body {
+    font-family: 'Roboto', sans-serif;
+  }
 
   .bg-secondary {
     background-color: hsl(208, 7%, 60%) !important;
@@ -207,9 +216,35 @@ const runewords = computed(() => {
     color: #555;
     font-weight: 700;
   }
+  
+  .runeword-display {
+    line-height: 1.25;
+  }
 
   .unique-title {
-    color: hsl(51, 24%, 65%);
+    color: hsl(51, 24%, 51%);
+    font-family: 'AvQuest';
+    font-size: 1.25em;
+    font-weight: 700;
+    letter-spacing: 1px;
+  }
+  
+  .rune-list {
+    color: hsl(45, 100%, 45%);
+    font-family: 'AvQuest';
+    font-size: 1.1em;
+    font-weight: 700;
+    letter-spacing: 1px;
+  }
+
+  .item-type-list {
+    color: hsl(0, 0%, 76%);
+    font-size: 1.1em;
+    letter-spacing: 1px;
+  }
+
+  .magic-mod {
+    color: hsl(216, 98%, 66%);
   }
 
 </style>
